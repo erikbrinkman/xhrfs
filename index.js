4e033b3f565c41d0b393bb714fa469e2ce433cfa
Erik Brinkman <erik.brinkman@gmail.com>
2017-08-10 18:34:44 -0400

    Address some major problems

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 'use strict';
 const path = require('path');
-// FIXME Handle errors
 
-function process(response, options) {
-  if (options === 'utf8' || (options || {}).encoding === 'utf8') {
-    return response;
+function getEncoding(options) {
+  const type = typeof options;
+  if (!options) {
+    return options;
+  } else if (type === 'string' || options instanceof String) {
+    return options;
+  } else if (type == 'object') {
+    return options.encoding;
   } else {
-    return Buffer.from(response);
+    throw new TypeError(
+      `"options" must be a string or an object, got ${type} instead.`)
   }
 }
 
 function readFile(filePath, options, callback) {
+  if (typeof filePath !== 'string' && !(filePath instanceof String)) {
+    throw new TypeError("path must be a string");
+  }
   if (callback === undefined) {
     callback = options;
     options = undefined;
   }
+  const encoding = getEncoding(options);
   const req = new XMLHttpRequest();
   req.open('GET', 'file://' + path.resolve(filePath));
-  req.addEventListener('readystatechange', () =>     {
-    if(req.readyState === 4) {
-      if(req.status === 200 || req.status === 0) {
-        callback(null, process(req.responseText, options));
+  req.responseType = 'arraybuffer';
+  req.addEventListener('readystatechange', () => {
+    if (req.readyState === 4) {
+      if (req.status === 200 || req.status === 0) {
+        const buff = Buffer.from(req.response);
+        if (encoding) {
+          try {
+            callback(null, buff.toString(encoding));
+          } catch (ex) {
+            callback(ex, null);
+          }
+        } else {
+          callback(null, buff);
+        }
       } else {
+        // FIXME Handle errors
         callback(req.statusText, null);
       }
     }
@@ -30,12 +50,21 @@ function readFile(filePath, options, callback) {
 }
 
 function readFileSync(filePath, options) {
+  if (typeof filePath !== 'string' && !(filePath instanceof String)) {
+    throw new TypeError("path must be a string");
+  }
+  const encoding = getEncoding(options);
+  if (encoding !== 'utf8') {
+    throw new Error(
+      `only utf8 encoding is allowed for synchronous reads got ${encoding}`);
+  }
   const req = new XMLHttpRequest();
   req.open('GET', 'file://' + path.resolve(filePath), false);
   req.send();
-  if(req.status === 200 || req.status === 0) {
-    return process(req.responseText, options);
+  if (req.status === 200 || req.status === 0) {
+    return req.responseText;
   } else {
+    // FIXME Handle errors
     throw req.statusText;
   }
 }
